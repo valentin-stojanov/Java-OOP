accc758c6473aa50410b4c7fb6934fcc2cd89427
valentin <valentin_stojanov@abv.bg>
2021-07-09 21:18:51 +0300

    Added TrafficLight exercise

--- Exercise/WirkingWithAbstraction/src/TrafficLights/Main.java
+++ Exercise/WirkingWithAbstraction/src/TrafficLights/Main.java
@@ -1,7 +1,27 @@
 package TrafficLights;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        TrafficLight[] trafficLights = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(TrafficLight::valueOf)
+                .toArray(TrafficLight[]::new);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < trafficLights.length; j++) {
+               trafficLights[j] = TrafficLight.change(trafficLights[j]);
+                System.out.print(trafficLights[j] + " ");
+            }
+            System.out.println();
+        }
 
     }
+
+
 }
--- Exercise/WirkingWithAbstraction/src/TrafficLights/TrafficLight.java
+++ Exercise/WirkingWithAbstraction/src/TrafficLights/TrafficLight.java
@@ -1,5 +1,24 @@
 package TrafficLights;
 
-public class TrafficLight {
+public enum TrafficLight {
+    RED,
+    GREEN,
+    YELLOW,
+    ;
 
+
+    public static TrafficLight change(TrafficLight trafficLight) {
+        switch (trafficLight) {
+            case YELLOW:
+                trafficLight = RED;
+                break;
+            case RED:
+                trafficLight = GREEN;
+                break;
+            case GREEN:
+                trafficLight = YELLOW;
+                break;
+        }
+        return trafficLight;
+    }
 }
