4b8b55f2a7e8d2177c39bdb49b3b370cfbb89395
valentin-stojanov <valentin_stojanov@abv.bg>
2021-07-14 14:29:45 +0300

    Added randomArrayList exercise.

--- Lab/Inheritance/src/randomArrayList/RandomArrayList.java
+++ Lab/Inheritance/src/randomArrayList/RandomArrayList.java
@@ -6,7 +6,6 @@ import java.util.Random;
 
 public class RandomArrayList extends ArrayList<Object> {
 
-
     public Object getRandomElement(){
         int index = new Random().nextInt(super.size());
         return super.get(index);
