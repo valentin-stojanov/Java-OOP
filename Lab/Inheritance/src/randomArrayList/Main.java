7488946c9419d46bbf872e540b895be5c99492d7
valentin-stojanov <valentin_stojanov@abv.bg>
2021-07-14 14:29:01 +0300

    Added randomArrayList exercise.

---
+++ Lab/Inheritance/src/randomArrayList/Main.java
@@ -0,0 +1,9 @@
+package randomArrayList;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Lab/Inheritance/src/randomArrayList/RandomArrayList.java
@@ -0,0 +1,15 @@
+package randomArrayList;
+
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RandomArrayList extends ArrayList<Object> {
+
+
+    public Object getRandomElement(){
+        int index = new Random().nextInt(super.size());
+        return super.get(index);
+    }
+
+}
