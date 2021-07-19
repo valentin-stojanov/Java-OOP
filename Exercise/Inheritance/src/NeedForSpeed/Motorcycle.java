4586712b39e89bb911c9992cffc86e16d2238542
valentin-stojanov <valentin_stojanov@abv.bg>
2021-07-19 12:37:17 +0300

    Changes to NeedForSpeed exercise.

--- Exercise/Inheritance/src/NeedForSpeed/Motorcycle.java
+++ Exercise/Inheritance/src/NeedForSpeed/Motorcycle.java
@@ -1,11 +1,9 @@
 package NeedForSpeed;
 
 public class Motorcycle extends Vehicle {
-    public static final double DEFAULT_FUEL_CONSUMPTION = 10;
 
     public Motorcycle(double fuel, int horsepower) {
         super(fuel, horsepower);
-        super.setFuelConsumption(DEFAULT_FUEL_CONSUMPTION);
     }
 }
 
