5ef4e54a5d25105dcd161ef5f5980ffe03b78e07
valentin-stojanov <valentin_stojanov@abv.bg>
2021-08-06 16:54:02 +0300

    Added GettersAndSetters package.

---
+++ Lab/ReflectionAndAnnotation/src/GettersAndSetters/Main.java
@@ -0,0 +1,15 @@
+package GettersAndSetters;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Class<Reflection> clazz = Reflection.class;
+
+
+
+
+    }
+}
