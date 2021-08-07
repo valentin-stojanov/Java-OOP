68ed22ad6c2c087b368402832418bd2cd0940910
valentin <valentin_stojanov@abv.bg>
2021-08-07 17:49:31 +0300

    Added harvestingFields ReflectionAndAnnotation exercise.

--- Exercise/ReflectionAndAnnotation/src/harvestingFields/Main.java
+++ Exercise/ReflectionAndAnnotation/src/harvestingFields/Main.java
@@ -1,7 +1,28 @@
 package harvestingFields;
 
-public class Main {
-	public static void main(String[] args) {
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Scanner;
 
-	}
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String command = scanner.nextLine();
+        Field[] fields = RichSoilLand.class.getDeclaredFields();
+
+        StringBuilder builder = new StringBuilder();
+        while (!command.equals("HARVEST")) {
+            String modifier = command;
+            Arrays.stream(fields)
+                    .filter(f -> Modifier.toString(f.getModifiers()).equals(modifier) || modifier.equals("all") )
+                    .forEach(field -> {
+                        builder.append(String.format("%s %s %s%n", Modifier.toString(field.getModifiers()), field.getType().getSimpleName() ,field.getName()));
+                    });
+
+            command = scanner.nextLine();
+        }
+        System.out.println(builder.toString());
+    }
 }
